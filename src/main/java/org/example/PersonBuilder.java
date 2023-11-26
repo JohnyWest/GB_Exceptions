06dd8c612a3d50b0980a46c4b65739d7b3e2398e
Johny_West <bbassed@gmail.com>
2023-11-26 22:48:42 +0500

    Setted up PersonBuilder

--- src/main/java/org/example/PersonBuilder.java
+++ src/main/java/org/example/PersonBuilder.java
@@ -1,2 +1,30 @@
 public class PersonBuilder {
+    private Person person;
+    public PersonBuilder() {
+        person = new Person();
+    }
+    public Person build(){
+        return person;
+    }
+
+    public PersonBuilder setLastname(String lastname) {
+        person.lastname = lastname;
+        return this;
+    }
+    public PersonBuilder setFatherName (String fatherName) {
+        person.fatherName = fatherName;
+        return this;
+    }
+    public PersonBuilder setdOb(String dOb) {
+        person.dOb = dOb;
+        return this;
+    }
+    public PersonBuilder setNumber (Long number) {
+        person.number = number;
+        return this;
+    }
+    public PersonBuilder setGender (String gender) {
+        person.gender = gender;
+        return this;
+    }
 }
