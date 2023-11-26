c6f5ec9eec5825b89b27f51da3e91c6d219cb22a
Johny_West <bbassed@gmail.com>
2023-11-26 22:47:53 +0500

    Setted upd PersonBuilder

--- src/main/java/org/example/BoxPersonalData.java
+++ src/main/java/org/example/BoxPersonalData.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class BoxPersonalData {
+public class BoxPersonalData {
+    String[] personalData;
+    public String[] getPersonalData() {
+        return personalData;
+    }
+    public BoxPersonalData(String personalData) {
+        this.personalData = personalData.split(" ");
+    }
+    public int lenDataOK() {
+        if (personalData.length != 4) {
+            System.out.println("Введено меньше строк, чем необходимо");
+            return -1;
+        }
+        return 0;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,9 @@ public class Main {
         System.out.println("Введите ваши данные в формате: Фамилия,Имя,Отчество ДатаРождения(через\".\") пол(m/f) телефон");
         System.out.println("Например: Иванов,Иван,Иванович 01.01.1970 m 89991199990");
         String personalData = in.nextLine();
+        BoxPersonalData boxPersonalData1 = new BoxPersonalData(personalData);
+
+
 
     }
 }
